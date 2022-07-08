7462c6cca541577ac776e9518038522878e30a0a
RobertsonTanya <bytanyar@gmail.com>
2022-07-08 06:25:10 -0400

    FE Start

---
+++ client/src/components/CreateNew.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const CreateNew = () => {
+    
+    return (
+        <div>
+            New Recipe
+        </div>
+    )
+}
+
+export default CreateNew;
---
+++ client/src/components/Details.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
+const Details = () => {
+    const { id } = useParams();
+
+    return (
+        <div>
+            Details
+        </div>
+    )
+}
+
+export default Details;
